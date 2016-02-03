b12ea67aa9b01f780d91cf3c99cdb46e9aed7120
Olivier Chafik <ochafik@users.noreply.github.com>
2016-02-03 16:53:43 +0000

    Update async_set.dart

--- lib/src/async/async_set.dart
+++ lib/src/async/async_set.dart
@@ -4,7 +4,8 @@ import 'package:quiver/streams.dart' as streams;
 
 class AsyncSet<T> {
   final _set = new Set<T>();
-  final _completers = <T, Completer>{};
+  final _completers = <T, Completer<bool>>{};
+  final _completer = new Completer();
   final Stream<T> _stream;
 
   AsyncSet(this._stream) {
@@ -13,20 +14,21 @@ class AsyncSet<T> {
       _completers.remove(value)?.complete(true);
     }, onDone: () {
       _completers.values.forEach((c) => c.complete(false));
+      _completer.complete();
     });
   }
+
+  Future<Set<T>> toSet() => _completer.future.then((_) => _set);
 
   Future<bool> contains(T value) {
     if (_set.contains(value)) {
       return new Future.value(true);
     }
-    return _completers.putIfAbsent(value, () => new Completer()).future;
+    return _completers.putIfAbsent(value, () => new Completer<bool>()).future;
   }
 
   Stream<T> toStream() {
-    return streams.concat([
-      new Stream<T>.fromIterable(_set.toList(growable: false)),
-      _stream
-    ]);
+    return streams.concat(
+        [new Stream<T>.fromIterable(_set.toList(growable: false)), _stream]);
   }
 }
